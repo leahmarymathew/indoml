cfee8087e316d0cd280f619c27dbada2e7c95432
Leah <70711778+leahmarymathew@users.noreply.github.com>
2025-10-25 22:12:39 +0530

    Refactor MPI program for message passing
    Updated the program to send and receive messages between two processes using MPI.

--- j.c
+++ j.c
@@ -1,36 +1,50 @@
 #include <mpi.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
-    int rank, np;
-    const int N = 4;
-    char B[N][N];
-    char local_row[N];
+    int rank, size;
+    const int MAX_STRING = 100;
+    char message1[MAX_STRING];
+    char message2[MAX_STRING];
+    MPI_Status status;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (np != N) {
+    if (size < 2) {
         if (rank == 0) {
-            printf("This program must be run with exactly %d processes.\n", N);
-            printf("Please run again: mpirun -np %d ./your_program_name\n", N);
+            printf("Error: This program requires at least 2 processes.\n");
+            printf("Run with: mpirun -np 2 ./your_program_name\n");
         }
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
     if (rank == 0) {
-        B[0][0] = 'A'; B[0][1] = 'B'; B[0][2] = 'C'; B[0][3] = 'D';
-        B[1][0] = 'E'; B[1][1] = 'F'; B[1][2] = 'G'; B[1][3] = 'H';
-        B[2][0] = 'I'; B[2][1] = 'J'; B[2][2] = 'K'; B[2][3] = 'L';
-        B[3][0] = 'M'; B[3][1] = 'N'; B[3][2] = 'O'; B[3][3] = 'P';
+        strcpy(message1, "Chandrayaan3");
+        strcpy(message2, "23-August-2023");
+        
+        printf("Process 0 (ISRO) sending: %s\n", message1);
+        MPI_Send(message1, strlen(message1) + 1, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+        
+        printf("Process 0 (ISRO) sending: %s\n", message2);
+        MPI_Send(message2, strlen(message2) + 1, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
+        
+        printf("Process 0 (ISRO) has sent all data.\n");
+
+    } else if (rank == 1) {
+        
+        printf("Process 1 (Moon) is waiting for data...\n");
+        
+        MPI_Recv(message1, MAX_STRING, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+        printf("Process 1 (Moon) received: %s\n", message1);
+        
+        MPI_Recv(message2, MAX_STRING, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &status);
+        printf("Process 1 (Moon) received: %s\n", message2);
+        
+        printf("Process 1 (Moon) has received all data.\n");
     }
-
-    MPI_Scatter(B, N, MPI_CHAR, local_row, N, MPI_CHAR, 0, MPI_COMM_WORLD);
-
-    printf("Process %d received row: %c, %c, %c, %c\n", 
-           rank, local_row[0], local_row[1], local_row[2], local_row[3]);
 
     MPI_Finalize();
     return 0;
