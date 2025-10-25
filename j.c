8274e3aad32bf206ff0de2fe4b28b482c3314ca4
Leah <70711778+leahmarymathew@users.noreply.github.com>
2025-10-25 21:10:16 +0530

    Update j.c

--- j.c
+++ j.c
@@ -3,12 +3,25 @@
 
 int main(int argc, char** argv) {
     int rank;
+    int a = 9; 
+    int b = 7;
+    int c;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    printf("Rank %d: Name: Leah, Roll Number: 2023BCS0190\n", rank);
+    if (rank == 0) {
+        c = a & b;
+        printf("Rank %d: c = a & b = %d\n", rank, c);
+    } else if (rank == 1) {
+        c = a | b;
+        printf("Rank %d: c = a | b = %d\n", rank, c);
+    } else {
+        c = a ^ b;
+        printf("Rank %d: c = a ^ b = %d\n", rank, c);
+    }
 
     MPI_Finalize();
     return 0;
 }
+
